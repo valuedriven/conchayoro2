c0a445a43374cc4a44fc5f88266327a8aa053572
valuedriven <junilson@hotmail.com>
2020-06-16 08:24:05 -0300

    Refatoração  para uso no AWS

--- client/vue.config.js
+++ client/vue.config.js
@@ -7,7 +7,7 @@ require('dotenv').config({
 module.exports = {
 
     devServer: {      
-      host: process.env.NODE_ENV === 'production' ? '' : process.env.VUE_APP_PORT,
+      host: process.env.NODE_ENV === 'production' ? '' : process.env.VUE_APP_HOST,
       port: process.env.NODE_ENV === 'production' ? '' : process.env.VUE_APP_PORT,
       https: false,
       hotOnly: false,
