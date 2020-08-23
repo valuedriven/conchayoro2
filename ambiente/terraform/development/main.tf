d6b6c1e8cfbd7ca5c4aa8397624f48bf8f8987ea
valuedriven <junilson@hotmail.com>
2020-08-23 12:01:42 -0300

    Ajustes configuração conteineres

--- ambiente/terraform/development/main.tf
+++ ambiente/terraform/development/main.tf
@@ -3,13 +3,18 @@ resource "null_resource" "DeployDev" {
   provisioner "local-exec" {
     command = "sh ambiente/terraform/development/excluirContainerLocal.sh"
   }
+
   provisioner "local-exec" {
-    command = "docker run --rm -d --env-file ${var.envFileDB} --hostname ${var.DBHost} --name ${var.DBHost} -p ${var.DBPort}:${var.DBContainerPort} ${var.imageDB}"
+    command = "docker network create localNetwork || true"
+  }
+
+  provisioner "local-exec" {
+    command = "docker run --rm -d --env-file ${var.envFileDB} --hostname ${var.DBHost} --name ${var.DBHost} --network localNetwork -p ${var.DBPort}:${var.DBContainerPort} ${var.imageDB}"
   }
   provisioner "local-exec" {
-    command = "docker run --rm -d --env-file ${var.envFileServer} --hostname ${var.ServerHost} --name ${var.ServerHost} -p ${var.ServerPort}:${var.ServerContainerPort} ${var.imageServer}"
+    command = "docker run --rm -d --env-file ${var.envFileServer} --hostname ${var.ServerHost} --name ${var.ServerHost} --network localNetwork  -p ${var.ServerPort}:${var.ServerContainerPort} ${var.imageServer}"
   }
   provisioner "local-exec" {
-    command = "docker run --rm -d --env-file ${var.envFileClient} --hostname ${var.ClientHost} --name ${var.ClientHost} -p ${var.ClientPort}:${var.ClientContainerPort} ${var.imageClient}"
+    command = "docker run --rm -d --env-file ${var.envFileClient} --hostname ${var.ClientHost} --name ${var.ClientHost} --network localNetwork  -p ${var.ClientPort}:${var.ClientContainerPort} ${var.imageClient}"
   }
 }
